d7b9db4f368c7cb56b84a8d2e8595ba1e4ca2d5c
dev-sandeep <sandip.pa@cisinlabs.com>
2026-01-12 19:24:35 +0530

    remove currently everythink from main branch

--- HRM-MICROSERVICE-BACKEND/services/auth-service/src/bootstrap/db.bootstrap.ts
+++
@@ -1,5 +0,0 @@
-import { connectToDatabase } from "../infrastructure/database/mongodb.connection";
-
-export async function initializeDatabase(): Promise<void> {
-  await connectToDatabase();
-}
--- HRM-MICROSERVICE-BACKEND/services/auth-service/src/server.ts
+++
@@ -1,9 +0,0 @@
-import { startHttpServer } from './bootstrap/http.bootstrap';
-import { initializeDatabase } from './bootstrap/db.bootstrap';
-
-async function main() {
-  await startHttpServer();
-  await initializeDatabase();
-}
-main();
-
